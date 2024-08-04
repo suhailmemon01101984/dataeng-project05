02a4e1bbcb276db84972e65f6ab4d419141dc762
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-08-03 18:33:51 -0600

    upload_files_to_s3.py

--- upload_files_to_s3.py
+++ upload_files_to_s3.py
@@ -2,10 +2,17 @@ import boto3
 import os
 
 
-def s3_upload_file(file, bucket):
+def s3_upload_raw_stats_file(file, bucket):
     filename = f'./datafiles/{file}'
+    bucket_file_name=f'youtube/raw_statistics_reference_data/{file}'
     s3 = boto3.client('s3')
-    s3.upload_file(filename, bucket, file)
+    s3.upload_file(filename, bucket, bucket_file_name)
+
+def s3_upload_region_data_file(file, bucket,region):
+    filename = f'./datafiles/{file}'
+    bucket_file_name=f'youtube/raw_statistics/region={region}/{file}'
+    s3 = boto3.client('s3')
+    s3.upload_file(filename, bucket, bucket_file_name)
 
 
 bucketname = 'suhailmemon84-youtube-de-project'
@@ -13,4 +20,8 @@ bucketname = 'suhailmemon84-youtube-de-project'
 directory = os.fsencode('./datafiles')
 for file in os.listdir(directory):
     file_name = os.fsdecode(file)
-    s3_upload_file(file_name, bucketname)
+    if file_name.endswith(".json"):
+        s3_upload_raw_stats_file(file_name, bucketname)
+    if file_name.endswith(".csv"):
+        s3_upload_region_data_file(file_name,bucketname,file_name[:2].lower())
+
