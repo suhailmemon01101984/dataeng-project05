f876c737979a01706b1bbac0c976384a43916dd1
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-10-27 14:08:21 -0600

    lambda function

--- lamdba_function.py
+++ lamdba_function.py
@@ -9,16 +9,22 @@
 # under bucket, change name as "name": "suhailmemon84-youtube-de-project"
 # make arn as "arn": "arn:aws:s3:::suhailmemon84-youtube-de-project"
 # make key under object as: "key": "youtube/raw_statistics_reference_data/CA_category_id.json",
+# go under function configuration and add environment variables: env_clean_s3_path, env_glue_database, env_glue_table, env_glue_table_write_mode
+# with values: s3://suhailmemon84-youtube-de-project-cleaned/youtube, suhailmemon84-dev, cleaned_stats_reference_data and append
 #then deploy your function and click the test button
 
 import json
+import os
 import boto3
 import awswrangler as wr
 import pandas as pd
 
 def lambda_handler(event, context):
     # TODO implement
-
+    env_clean_s3_path=os.getenv('env_clean_s3_path')
+    env_glue_database=os.getenv('env_glue_database')
+    env_glue_table=os.getenv('env_glue_table')
+    env_glue_table_write_mode=os.getenv('env_glue_table_write_mode')
     bucketname=event["Records"][0]["s3"]["bucket"]["name"]
     bucketfilename=event["Records"][0]["s3"]["object"]["key"]
     s3filepath=f's3://{bucketname}/{bucketfilename}'
@@ -28,10 +34,11 @@ def lambda_handler(event, context):
     #print(df_items)
     #df_items=df_json["items"]
     #print(type(df_items))
-    wr.s3.to_parquet(df=df_items, path='s3://suhailmemon84-youtube-de-project-cleaned/youtube', dataset=True, database='suhailmemon84-dev', table='reference_data_cleaned_stats', mode='append' )
+    wr.s3.to_parquet(df=df_items, path=env_clean_s3_path, dataset=True, database=env_glue_database, table=env_glue_table, mode=env_glue_table_write_mode )
 
 
     return {
         'statusCode': 200,
         'body': json.dumps('Suhail, Hello from Lambda!')
     }
+
